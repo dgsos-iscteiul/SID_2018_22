7cc0de8c5546179b7b68b39b64734e6fee9e8970
sfves-iscteiul <sfves@iscte-iul.pt>
2019-05-13 16:32:39 +0100

    Initial Junit tests

---
+++ src/testesJunit/MySqlConnectionTest.java
@@ -0,0 +1,19 @@
+package testesJunit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runners.JUnit4;
+
+import db_config.MySqlConnection;
+
+class MySqlConnectionTest {
+
+	MySqlConnection connection = new MySqlConnection();
+	
+	@Test
+	void test() {
+		
+		}
+
+}
