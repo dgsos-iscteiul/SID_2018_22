f6d5a34076c9ea7ee5c4ff3878ed863a219fd888
dgsos-iscteiul <dgsos@iscte-iul.pt>
2019-05-13 17:58:29 +0100

    Ola

---
+++ src/testesJunit/UserTest.java
@@ -0,0 +1,40 @@
+package testesJunit;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.*;
+
+import config.User;
+
+public class UserTest {
+
+	private User user;
+
+	@Before
+	public void executedBeforeEach() {
+		user = new User("testUser", "testPW");
+	}
+
+
+	@Test
+	public void testGetName() {
+		assertEquals("testUser", user.getName());
+	}
+	
+	@Test
+	public void testSetName() {	
+		user.setName("testUsertest");
+		assertEquals("testUsertest", user.getName());
+	}
+	
+	@Test
+	public void testGetPassword() {
+		assertEquals("testPW", user.getPassword());
+	}
+	
+	
+
+
+}
