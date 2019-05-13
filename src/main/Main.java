3092a6c2f72cfb981c26609bbdee7b34f6d33d68
ajcvo-iscteiul <ajcvo@iscte-iul.pt>
2019-05-13 23:33:51 +0100

    Main adicionado

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import gui.geral.LoginFrame;
+
+public class Main {
+	/** Initiates the software. */
+	public static void main(String[] args) {
+		LoginFrame lf = new LoginFrame();
+	}
+}
